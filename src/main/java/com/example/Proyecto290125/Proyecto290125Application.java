98795a63960ae079df6a24131e9007ea9faba3ef
wandaglezest <wanda.glez.est@gmail.com>
2025-01-29 09:09:49 +0100

    Add files via upload
    Commit 2

---
+++ src/main/java/com/example/Proyecto290125/Proyecto290125Application.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto290125;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto290125Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto290125Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Proyecto290125/Proyecto290125ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Proyecto290125;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto290125ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
